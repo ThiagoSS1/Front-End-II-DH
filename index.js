3efc2b69ff8f5add8492dbef31ad608a4c5bdab7
Thiago Seta <thiago_seta@hotmail.com>
2023-02-14 08:58:56 -0300

    subindo repo

---
+++ index.js
@@ -0,0 +1,8 @@
+let userName = prompt("Qual o seu nome?");
+let userAge = prompt("Qual a sua idade?");
+let userJob = prompt("Qual a sua profissão?");
+
+const message = `Olá me chamo ${userName}, tenho ${userAge} anos e atualmente trabalho como ${userJob}`
+
+alert(message);
+
